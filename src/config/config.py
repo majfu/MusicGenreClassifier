90382ce3bb927b9fbcf32d249f7cfc82a023fd5f
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2025-07-28 18:20:20 +0200

    Changed folder names

--- src/config/config.py
+++ src/config/config.py
@@ -3,16 +3,16 @@ SHOULD_DELETE_MP3 = True
 CONTENT_DRIVE_PATH = '/content/drive'
 PROJECT_FOLDER_PATH = '/content/drive/MGCProject'
 
-METADATA_ZIP_FILE_PATH = '/content/drive/MGCProject/metadata.zip'
-TRACKS_METADATA_FILE_PATH = '/content/drive/MGCProject/metadata/tracks.csv'
-GENRES_METADATA_FILE_PATH = '/content/drive/MGCProject/metadata/genres.csv'
+METADATA_ZIP_FILE_PATH = '/content/drive/MGCProject/fma_metadata.zip'
+TRACKS_METADATA_FILE_PATH = '/content/drive/MGCProject/fma_metadata/tracks.csv'
+GENRES_METADATA_FILE_PATH = '/content/drive/MGCProject/fma_metadata/genres.csv'
 
 FMA_SMALL_ZIP_PATH = '/content/drive/MGCProject/fma_small.zip'
 FMA_SMALL_FOLDER_PATH = '/content/drive/MGCProject/fma_small'
 
-FEATURE_VECTORS_FOLDER_PATH = '/content/drive/MGCProject/extractedFeatures'
+FEATURE_VECTORS_FOLDER_PATH = '/content/drive/MGCProject/fma_small_features'
 
-ENCODED_LABELS_OUTPUT_PATH = '/content/drive/MGCProject/metadata/labels.csv'
-TRAIN_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/metadata/train.csv'
-VAL_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/metadata/val.csv'
-TEST_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/metadata/test.csv'
+ENCODED_LABELS_OUTPUT_PATH = '/content/drive/MGCProject/fma_metadata/labels.csv'
+TRAIN_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/fma_metadata/train.csv'
+VAL_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/fma_metadata/val.csv'
+TEST_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/fma_metadata/test.csv'
