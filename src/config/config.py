8a89d7b89e65781b897e7c0b8a9c3f0fcbf84bff
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2025-08-01 19:35:30 +0200

    Corrected the paths

--- src/config/config.py
+++ src/config/config.py
@@ -1,18 +1,20 @@
 SHOULD_DELETE_MP3 = True
 
 CONTENT_DRIVE_PATH = '/content/drive'
-PROJECT_FOLDER_PATH = '/content/drive/MGCProject'
+PROJECT_FOLDER_PATH = '/content/drive/MyDrive/MGCProject'
 
-METADATA_ZIP_FILE_PATH = '/content/drive/MGCProject/fma_metadata.zip'
-TRACKS_METADATA_FILE_PATH = '/content/drive/MGCProject/fma_metadata/tracks.csv'
-GENRES_METADATA_FILE_PATH = '/content/drive/MGCProject/fma_metadata/genres.csv'
+METADATA_ZIP_FILE_PATH = '/content/drive/MyDrive/MGCProject/fma_metadata.zip'
+TRACKS_METADATA_FILE_PATH = '/content/drive/MyDrive/MGCProject/fma_metadata/tracks.csv'
+GENRES_METADATA_FILE_PATH = '/content/drive/MyDrive/MGCProject/fma_metadata/genres.csv'
 
-FMA_SMALL_ZIP_PATH = '/content/drive/MGCProject/fma_small.zip'
-FMA_SMALL_FOLDER_PATH = '/content/drive/MGCProject/fma_small'
+FMA_SMALL_ZIP_PATH = '/content/drive/MyDrive/MGCProject/fma_small.zip'
+FMA_SMALL_FOLDER_PATH = '/content/drive/MyDrive/MGCProject/fma_small'
+AUDIO_FILES_FOLDER_PATH = '/content/drive/MyDrive/MGCProject/wav_audio_files'
 
-FEATURE_VECTORS_FOLDER_PATH = '/content/drive/MGCProject/fma_small_features'
+INDEX_MAPPING_FILE_PATH = '/content/drive/MyDrive/MGCProject/fma_metadata/index_mapping.csv'
+FEATURE_VECTORS_FOLDER_PATH = '/content/drive/MyDrive/MGCProject/fma_small_features'
 
-ENCODED_LABELS_OUTPUT_PATH = '/content/drive/MGCProject/fma_metadata/labels.csv'
-TRAIN_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/fma_metadata/train.csv'
-VAL_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/fma_metadata/val.csv'
-TEST_SPLIT_OUTPUT_PATH = '/content/drive/MGCProject/fma_metadata/test.csv'
+ENCODED_LABELS_OUTPUT_PATH = '/content/drive/MyDrive/MGCProject/fma_metadata/labels.csv'
+TRAIN_SPLIT_OUTPUT_PATH = '/content/drive/MyDrive/MGCProject/fma_metadata/train.csv'
+VAL_SPLIT_OUTPUT_PATH = '/content/drive/MyDrive/MGCProject/fma_metadata/val.csv'
+TEST_SPLIT_OUTPUT_PATH = '/content/drive/MyDrive/MGCProject/fma_metadata/test.csv'
