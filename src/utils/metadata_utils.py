fec45520ce7b49048722ef8c3bb2f722af4b0af1
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2025-08-25 18:06:00 +0200

    corrected poorly written functions

--- src/utils/metadata_utils.py
+++ src/utils/metadata_utils.py
@@ -13,26 +13,24 @@ def get_genre_ids_counts(track_id_genre_id_pairs):
     return track_id_genre_id_pairs['genre_id'].value_counts()
 
 
-def get_class_number(labels_df):
-    return len(labels_df.columns) - 1
+def get_num_samples(labels_df):
+    return len(labels_df)
 
 
-def get_samples_number(labels_df):
-    return len(labels_df) - 1
+def get_num_genres(labels_df):
+    return labels_df.shape[1]
 
 
 def get_class_counts(labels_df):
-    return labels_df.drop(columns=['track_id']).sum(axis=0)
+    return labels_df.sum(axis=0)
 
 
-def get_class_weights_tensor(encoded_labels_file_path=TRAIN_SPLIT_OUTPUT_PATH):
-    labels_df = load_encoded_labels_df(encoded_labels_file_path=encoded_labels_file_path)
-    class_number = get_class_number(labels_df)
-    samples_number = get_samples_number(labels_df)
+def convert_df_to_tensor(df):
+    return torch.tensor(df.to_numpy(), dtype=torch.float)
 
+
+def get_pos_weights_tensor(labels_df):
+    num_samples = get_num_samples(labels_df)
     class_counts = get_class_counts(labels_df)
-    class_frequencies = class_counts / samples_number
-    class_weights = 1 / class_frequencies
-    normalized_class_weights = class_weights / class_weights.sum() * class_number
-
-    return torch.from_numpy(normalized_class_weights.to_numpy())
+    pos_weights = (num_samples - class_counts) / class_counts
+    return convert_df_to_tensor(pos_weights)
